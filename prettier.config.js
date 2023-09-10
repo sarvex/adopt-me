abb260a5a03cae78afe9d4c013b04334ddf91be2
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2023-09-10 13:38:51 +0530

    Added Vite & React

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  bracketSameLine: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+};
+
+export default config;
